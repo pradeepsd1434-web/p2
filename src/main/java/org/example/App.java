86904dcb8bd664f242ce2b1d4ade63c3e7a57cd4
pradeepsd1434-web <pradeepsd1434@gmail.com>
2026-06-23 18:17:02 +0530

    added files

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,11 +6,10 @@ public class App {
         }
         public static void main(String []args){
             App app=new App ();
-            int result = app.add(2,5);
+            int result = app.add(2,3);
             System.out.println("2+3="+result);
             System.out.println("appplicatin excute succes");
 
 
-            git remote
     }
 }
